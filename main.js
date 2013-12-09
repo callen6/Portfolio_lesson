15c31420a84e962cde240a3cf85881510882d24a
Cori Allen <coriallen@gmail.com>
2013-12-09 16:09:05 -0500

    Added working navigation scroll or direct link with jsand jquery.

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 var Page = {
 	setNavigationClicks: function () {
 		$('.navigation-wrapper a').click(function(e) {
-			e.preventDefault();
+			if(!$(e.currentTarget).hasClass('direct-link'))
+		e.preventDefault();
+		Page.scrollToSection($(e.currentTarget).attr('href'));
 		});
+	},
+
+	scrollTo: function(yValue) {
+		$('html body').animate({
+			'scrollTop':yValue
+		}, 1000);
+	},
+
+	scrollToSection: function(sectionString) {
+		var sectionClass = "." + sectionString;
+		Page.scrollTo($(sectionClass).offset().top - 100);
 	}
 };
 
 $(function() {
 	Page.setNavigationClicks();
 })
+
